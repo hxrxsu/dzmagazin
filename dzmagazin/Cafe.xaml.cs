e1ab5cb3fdfa645a2100f1a43a8cb7fe726ff372
HXRXSU <HXRXSU@DESKTOP-5LLNC12>
2024-11-14 17:48:13 +0400

    Add project files.

---
+++ dzmagazin/Cafe.xaml.cs
@@ -0,0 +1,99 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dzmagazin
+{
+    /// <summary>
+    /// Interaction logic for Cafe.xaml
+    /// </summary>
+    public partial class Cafe : Page
+    {
+        public Cafe()
+        {
+            InitializeComponent();
+
+            LoadDataToListBox();
+        }
+        string filePath = "ordersinfofile.json";
+        private void BN_Add_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                string newItem = $"{TB_OrderName.Text} + {TB_Desc.Text} + {TB_Price.Text}";
+                if (!string.IsNullOrWhiteSpace(newItem))
+                {
+                    LB_Items.Items.Add(newItem);
+
+                    MessageBox.Show($"{newItem}");
+
+                    string json = ListBoxToJsonSerializer(LB_Items);
+                    File.WriteAllText(filePath, json);
+
+                    TB_OrderName.Clear();
+                    TB_Desc.Clear();
+                    TB_Price.Clear();
+                }
+            }
+            catch (Exception ex) { MessageBox.Show($"Ошибка! {ex}"); }
+        }
+
+        private string ListBoxToJsonSerializer(ListBox listBox)
+        {
+            var items = new List<string>();
+            foreach (var item in LB_Items.Items)
+            {
+                items.Add(item.ToString());
+            }
+            return JsonConvert.SerializeObject(items);
+        }
+
+        private void JsonToListboxDeserializer(ListBox listbox, string jsonString)
+        {
+            var items = JsonConvert.DeserializeObject<List<string>>(jsonString);
+            foreach (var item in items)
+            {
+                listbox.Items.Add(item);
+            }
+        }
+
+        private void LoadDataToListBox()
+        {
+            try
+            {
+                string json = File.ReadAllText(filePath);
+                JsonToListboxDeserializer(LB_Items, json);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при загрузке данных: {ex.Message}");
+            }
+        }
+        private void BN_Del_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                LB_Items.Items.RemoveAt(LB_Items.SelectedIndex);
+
+                string json = ListBoxToJsonSerializer(LB_Items);
+                File.WriteAllText(filePath, json);
+            }
+            catch(Exception ex) { }
+
+        }
+    }
+}
---
+++ dzmagazin/Registr.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dzmagazin
+{
+    /// <summary>
+    /// Interaction logic for Registr.xaml
+    /// </summary>
+    public partial class Registr : Page
+    {
+        public Registr()
+        {
+            InitializeComponent();
+        }
+
+        string filePath = "users.json";
+
+
+        private void BN_Auth_Click(object sender, RoutedEventArgs e)
+        {
+            List<Person> userdata = JsonConvert.DeserializeObject<List<Person>>(File.ReadAllText(filePath)) ?? new List<Person>();
+            var user = userdata.FirstOrDefault(u => u.login == TB_Login.Text && u.password == TB_Pass.Text);
+
+            if (user != null)
+            {
+                MessageBox.Show("Успешно! Привет " + user.login);
+
+                NavigationService.Navigate(new Cafe());
+            }
+            else
+            {
+                MessageBox.Show("Неправильный логин или пароль");
+            }
+        }
+
+        private void Label_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    File.Create(filePath).Close();
+                }
+
+
+
+
+                List<Person> userdata = JsonConvert.DeserializeObject<List<Person>>(File.ReadAllText(filePath)) ?? new List<Person>();
+                userdata.Add(new Person { login = TB_Login.Text, password = TB_Pass.Text });
+                string json = JsonConvert.SerializeObject(userdata);
+                File.WriteAllText(filePath, json);
+
+                MessageBox.Show("Вы зарегистрировались");
+            }
+            catch (Exception ex) { MessageBox.Show($"Ошибка {ex}"); }
+        }
+    }
+}
